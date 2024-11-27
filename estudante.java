8dce4042dd09ea1c69c863bb88d14ee6e4161cf4
Gutemberg Silva Santos <gutemsocial@gmail.com>
2024-11-27 12:49:45 -0300

    Add files via upload
    Adição de exibição de notas

--- estudante.java
+++ estudante.java
@@ -14,16 +14,64 @@ import java.util.List;
 class estudante extends pessoa {
     String matricula;
     curso curso;
-    List<Double> notas = new ArrayList<>();
+    private List<notas> notas;     
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(curso curso) {
+        this.curso = curso;
+    }
+
+    public List<notas> getNotas() {
+        return notas;
+    }
+
+    public void setNotas(List<notas
+            > notas) {
+        this.notas = notas;
+    }
 
     estudante(String nome, String cpf, String endereco, String telefone, String matricula, curso curso) {
         super(nome, cpf, endereco, telefone);
         this.matricula = matricula;
         this.curso = curso;
     }
+    
+    public void exibirDados(){
+        for (notas n : notas) {
+            System.out.println(getNome() + " - Cpf: " + getCpf() + " - End. " + getEndereco() + " - Tel.: " + getTelefone() + ". Matricula: " + matricula);
+            n.exibirDados();
+        }
+    }
 
     public void printDetails() {
         System.out.println("Estudante " + nome + " - CPF: " + cpf + " - Matrícula: " + matricula);
+    }
+
+    private String getNome() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private String getCpf() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private String getEndereco() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private String getTelefone() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
 
