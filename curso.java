91a0eacac7d67b71b956fd73362b36c77cb4b681
Gutemberg Silva Santos <gutemsocial@gmail.com>
2024-11-29 13:57:11 -0300

    Add files via upload
    A classe curso agora tem um coordenador vinculado

--- curso.java
+++ curso.java
@@ -2,19 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Projeto;
+package Projeto2;
 
 /**
  *
  * @author Gutem
  */
-class curso {
-    String nomeCurso;
-    int quantidadeSemestres;
+public class curso {
+    private String nome;
+    private turma turma;
+    private professor coordenador;
 
-    curso(String nomeCurso, int quantidadeSemestres) {
-        this.nomeCurso = nomeCurso;
-        this.quantidadeSemestres = quantidadeSemestres;
+    public curso(String nome, turma turma, professor coordenador) {
+        this.nome = nome;
+        this.turma = turma;
+        this.coordenador = coordenador;
+    }
+
+    public void imprimirInformacoes() {
+        System.out.println("Curso: " + nome);
+        System.out.println("Coordenador: " + coordenador);
+        turma.imprimir();
+    }
+
+    public void vincularCoordenador(professor coordenador) {
+        this.coordenador = coordenador;
     }
 }
 
